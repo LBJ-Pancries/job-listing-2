9317c942579e0ef7c58048b8ae7ab32d7157e846
liubingjie <breeder1121@hotmail.com>
2017-01-18 10:31:55 +0800

    implement step 5

--- app/models/job.rb
+++ app/models/job.rb
@@ -2,4 +2,15 @@ class Job < ApplicationRecord
   validates :title, presence: true
   validates :wage_upper_bound, presence: true
   validates :wage_lower_bound, presence: true
+
+
+  def publish!
+    self.is_hidden = false
+    self.save
+  end
+
+  def hide!
+    self.is_hidden = true
+    self.save
+  end
 end
