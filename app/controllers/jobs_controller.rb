43c5cf8f74f054d7ae4f0c44a44d24237f0cb6b2
liubingjie <breeder1121@hotmail.com>
2017-02-04 19:00:58 +0800

    better welcome/index & admin/jobs/index

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -1,37 +1,12 @@
 class JobsController < ApplicationController
-  before_action :authenticate_user!, only: [:new, :create, :update, :edit, :destroy]
-  def destroy
+  before_filter :authenticate_user!, only: [:new, :create, :update, :edit, :destroy]
+
+  def show
     @job = Job.find(params[:id])
 
-    @job.destroy
-
-    redirect_to jobs_path
-  end
-
-  def edit
-    @job = Job.find(params[:id])
-  end
-
-  def update
-    @job = Job.find(params[:id])
-    if @job.update(job_params)
-      redirect_to jobs_path
-    else
-      render :edit
-    end
-  end
-
-  def new
-    @job = Job.new
-  end
-
-  def create
-    @job = Job.new(job_params)
-
-    if @job.save
-      redirect_to jobs_path
-    else
-      render :new
+    if @job.is_hidden
+      flash[:warning] = 'This Job already archieved'
+      redirect_to root_path
     end
   end
 
@@ -46,18 +21,44 @@ class JobsController < ApplicationController
             end
   end
 
-  def show
-    @job = Job.find(params[:id])
+  #  def new
+  #    @job = Job.new
+  #  end
 
-    if @job.is_hidden
-      flash[:warning] = "This Job already archieved"
-      redirect_to root_path
-    end
-  end
+  #  def create
+  #    @job = Job.new(job_params)
+
+  #    if @job.save
+  #      redirect_to jobs_path
+  #    else
+  #      render :new
+  #    end
+  #  end
+
+  #  def edit
+  #    @job = Job.find(params[:id])
+  #  end
+
+  #  def update
+  #    @job = Job.find(params[:id])
+  #    if @job.update(job_params)
+  #      redirect_to jobs_path
+  #    else
+  #      render :edit
+  #    end
+  #  end
+
+  #  def destroy
+  #    @job = Job.find(params[:id])
+
+  #    @job.destroy
+  #
+  #    redirect_to jobs_path
+  #  end
 
   private
 
   def job_params
-    params.require(:job).permit(:title, :description, :wage_upper_bound, :wage_lower_bound, :contact_email,:is_hidden)
+    params.require(:job).permit(:title, :description, :wage_upper_bound, :wage_lower_bound, :contact_email, :is_hidden)
   end
 end
